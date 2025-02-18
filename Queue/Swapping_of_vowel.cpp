a0d75bcd73bfaf8478da3d9444f9786bbfe9824e
sumit <sumitrajput88956@gmail.com>
2025-02-19 01:16:19 +0530

    Swapping of vowel

---
+++ Queue/Swapping_of_vowel.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<stack>
+
+bool s(char ch)
+{
+    ch=tolower(ch);
+    return ch=='a'|| ch=='e' ||ch=='i' || ch=='o' || ch=='u';
+}
+
+string swa(string str)
+{
+
+
+stack<char>vowel;
+
+for(char ch:str)
+if()
+}
+
+void sta()
+string s="aeroplane"
+
+{
+    for(int i=0;i<size();i++)
+    {
+        if('a'||'e'||'i'||'o'||'u')
+        {
+
+        }
+    }
+}
+
+int main()
+{
+
+}
